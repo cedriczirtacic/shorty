9ced27bce07e730aba70b1342f5bb4c81475e8cd
cedriczirtacic <cicatriz.r00t@gmail.com>
2018-04-19 15:25:56 -0600

    I messed up this shit

--- shortyhttpd.go
+++ shortyhttpd.go
@@ -9,6 +9,7 @@ import (
 type ShortyHTTPd struct {
 	port    int           // shorty httpd port
 	address string        // shorty httpd address
+	domain  string        // shorty httpd domain
 	sslcert string        // SSL/TLS certificate
 	sslkey  string        // SSL/TLS private key
 	ss      *ShortyServer // pointer ShortyServer
